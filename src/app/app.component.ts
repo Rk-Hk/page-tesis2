7be8e9b9a612c6ed3979f5d6f1851f6cce1c2756
Rick BE <raul.balvin@vasslatam.com>
2020-06-16 19:43:46 -0500

    Subiendo mi documento

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+// import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'page-tesisII';
+  pageStruct : any ;
+
+  constructor(
+    // private _http: HttpClient
+  ) {
+    
+  }
+
+  // getStruct(){
+  //   this._http.get('./assets/mock/struct.json').pipe().subscribe(data => {
+  //     this.pageStruct = data;
+  //   });
+  // }
+ 
+  ngOnInit( ) {
+    // this.getStruct()
+  }
+
 }
